dad69212671f4da9b5aa32eb960f8b9a625384eb
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-16 16:07:39 +0300

    loader add

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,13 +5,23 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import HomePage from "../Pages/HomePage";
-import Usom from "../Components/Usom/Usom";
+import { lazy, Suspense } from "react";
+import Loader from "../assets/Loader";
+
+const Usoms = lazy(() => import("../Components/Usom/Usom"));
 
 export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<HomePage />} />
-      <Route path="/usom" element={<Usom />} />
+      <Route
+        path="/usom"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Usoms />
+          </Suspense>
+        }
+      />
     </Route>
   )
 );
