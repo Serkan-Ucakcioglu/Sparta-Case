3783797b99f6383d2f17dcf82a12277584157e21
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-16 12:57:07 +0300

    export add

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const getImg = async () => {
+export const getImg = async () => {
   const { data } = await axios.get("https://randomfox.ca/floof/");
-
   return data;
 };
 
-const getStoa = async () => {
+export const getStoa = async () => {
   const { data } = await axios.get(
     "https://api.themotivate365.com/stoic-quote"
   );
