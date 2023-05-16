50338633e3d7a51dea9880708e0755964d3109f6
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-16 13:05:22 +0300

    react router add

---
+++ src/router/router.jsx
@@ -0,0 +1,9 @@
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+export const router = createHashRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
