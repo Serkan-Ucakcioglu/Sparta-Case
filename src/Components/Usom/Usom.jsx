89178b572fcdf7d6d3964e1f6b4b0c300bed27e7
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-18 12:53:39 +0300

    updated

--- src/Components/Usom/Usom.jsx
+++ src/Components/Usom/Usom.jsx
@@ -15,7 +15,22 @@ function Usom() {
     document.title = "Usom List";
   }, [page]);
   return (
-    <div className="w-full flex justify-center mt-8">
+    <div className="w-full flex flex-col items-center mt-8">
+      <div className="flex justify-center items-center gap-x-3">
+        <button
+          onClick={() => setPage((page) => page - 1)}
+          disabled={page == 0}
+          className="border border-blue-500 bg-blue-500 text-white p-2 text-sm rounded mt-2 hover:bg-blue-700 disabled:opacity-50"
+        >
+          Prev Page
+        </button>
+        <button
+          onClick={() => setPage((page) => page + 1)}
+          className="border border-blue-500 bg-blue-500 text-white p-2 text-sm rounded mt-2 hover:bg-blue-700 disabled:opacity-50"
+        >
+          Next Page
+        </button>
+      </div>
       <div className="min-w-[500px]">
         {usom?.length > 1 && <UsomTable data={usom} />}
       </div>
